9d3321df618834f07eaab2489402848e10f0a923
David Lai <72419337+dlai211@users.noreply.github.com>
2021-10-07 13:43:08 -0700

    Update identity_clusterer.py

--- calo_cluster/clustering/identity_clusterer.py
+++ calo_cluster/clustering/identity_clusterer.py
@@ -6,4 +6,5 @@ class IdentityClusterer(BaseClusterer):
         super().__init__(use_semantic, ignore_semantic_labels=None)
 
     def cluster(self, event):
+        print('running identity_clusterer.py')
         return event.embedding
