cd3dde87bc0167dee25a141aebb7d6f98ec6b649
David Lai <72419337+dlai211@users.noreply.github.com>
2021-10-12 12:24:36 -0700

    Update base_clusterer.py

--- calo_cluster/clustering/base_clusterer.py
+++ calo_cluster/clustering/base_clusterer.py
@@ -1,5 +1,4 @@
 class BaseClusterer:
-    print("running base_clusterer.py")
     def __init__(self, use_semantic, ignore_semantic_labels):
         self.use_semantic = use_semantic
         self.ignore_semantic_labels = ignore_semantic_labels
