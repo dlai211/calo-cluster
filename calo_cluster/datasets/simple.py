9c08beda6b51d22aa74fd284f77258ee0552958b
David Lai <72419337+dlai211@users.noreply.github.com>
2021-10-12 12:30:14 -0700

    Update simple.py

--- calo_cluster/datasets/simple.py
+++ calo_cluster/datasets/simple.py
@@ -16,7 +16,6 @@ from .calo import CaloDataset
 
 
 class SimpleDataset(CaloDataset):
-    print('running simple.py SimpleDataset')
     def __init__(self, **kwargs):
         super().__init__(semantic_label='semantic_label',
                          instance_label='instance_label', weight='E', **kwargs)
@@ -24,14 +23,12 @@ class SimpleDataset(CaloDataset):
 
 @dataclass
 class SimpleDataModule(BaseDataModule):
-    print('running simple.py SimpleDataModule')
     data_dir: str
 
     feats: List[str]
     coords: List[str]
 
     def __post_init__(self):
-        print('simple.py SimpleDataModule/__post_init__')
         super().__post_init__()
 
         self.data_dir = Path(self.data_dir)
@@ -39,7 +36,6 @@ class SimpleDataModule(BaseDataModule):
 
     @property
     def files(self) -> list:
-        print('simple.py SimpleDataModule/files(self)')
         if self._files is None:
             self._files = []
             self._files.extend(
@@ -48,7 +44,6 @@ class SimpleDataModule(BaseDataModule):
 
     @staticmethod 
     def _generate_event(rng, l, n_noise, noise_scale, signal_scale):
-        print('simple.py SimpleDataModule/_generate_event')
         noise_x = rng.uniform(-l, l, size=n_noise)
         noise_y = rng.uniform(-l, l, size=n_noise)
         noise_z = rng.uniform(-l, l, size=n_noise)
@@ -76,7 +71,6 @@ class SimpleDataModule(BaseDataModule):
 
     @staticmethod
     def generate(data_dir, l=10, n_noise=1000, noise_scale=0.5, signal_scale=10.0, n_events=10000) -> None:
-        print('simple.py SimpleDataModule/generate')
         logging.info(f'Generating data at {data_dir}.')
         rng = np.random.default_rng()
         for i in tqdm(range(n_events)):
@@ -85,12 +79,10 @@ class SimpleDataModule(BaseDataModule):
             df.to_pickle(event_path)
 
     def make_dataset(self, files: List[Path], split: str) -> SimpleDataset:
-        print('simple.py SimpleDataModule/make_dateset')
         kwargs = self.make_dataset_kwargs()
         return SimpleDataset(files=files, **kwargs)
 
     def make_dataset_kwargs(self) -> dict:
-        print('simple.py SimpleDataModule/make_dateset_kwargs')
         kwargs = {
             'feats': self.feats,
             'coords': self.coords
